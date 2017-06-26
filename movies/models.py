3bbc599b01adddeea507d337abefef19174a4a43
Weder Ribas <wederribas@gmail.com>
2017-06-26 00:13:26 -0300

    Create movies trailer model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MoviesTrailer(models.Model):
+	name = models.CharField(max_length=100)
+	year = models.IntegerField()
+	sinopse = models.CharField(max_length=300)
+	category = models.CharField(max_length=15)
+	trailer_url = models.CharField(max_length=200)
+
+	def __str__(self):
+		return str(self.name)
+
+	def get_movie_trailer(self):
+		return self.trailer_url
